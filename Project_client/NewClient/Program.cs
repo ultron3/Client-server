bfb2814e3c6cfd0373dce9887e7edf6a7beb2de4
ultron3 <alex.gnavi@gmail.com>
2023-08-08 19:18:11 +0200

    modifica lato client

--- Project_client/NewClient/Program.cs
+++ Project_client/NewClient/Program.cs
@@ -2,121 +2,67 @@
 using System.IO;
 using System.Net.Sockets;
 
-namespace NewClient
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Client
 {
-    public class Program
+    private static TcpClient client;
+    private static NetworkStream stream;
+
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        string serverIP = "192.168.3.232";
+        int serverPort = 5555;
+
+        client = new TcpClient();
+        client.Connect(serverIP, serverPort);
+
+        Console.WriteLine("Connected to the server.");
+
+        stream = client.GetStream();
+
+        Thread receiveThread = new Thread(ReceiveMessages);
+        receiveThread.Start();
+
+        string message;
+        while (true)
         {
-            string serverIP = "192.168.3.232";
-            int serverPort = 5555;
+            message = Console.ReadLine();
 
-            TcpClient client = new TcpClient();
-            client.Connect(serverIP, serverPort);
-
-            Console.WriteLine("Connected to the server.");
-
-            NetworkStream stream = client.GetStream();
-            StreamReader reader = new StreamReader(stream);
-            StreamWriter writer = new StreamWriter(stream);
-
-            // Leggi il messaggio di benvenuto dal server
-            string welcomeMessage = reader.ReadLine();
-            Console.WriteLine(welcomeMessage);
-
-            // Inizializza una nuova istanza del gestore dei messaggi in uscita
-            MessageSender messageSender = new MessageSender(writer);
-
-            // Avvia un thread separato per gestire la lettura dei messaggi in arrivo dal server
-            MessageReceiver messageReceiver = new MessageReceiver(reader);
-            messageReceiver.MessageReceived += (sender, receivedMessage) =>
+            if (message.ToLower() == "exit")
             {
-                Console.WriteLine("Message from the server: " + receivedMessage);
-            };
-            messageReceiver.Start();
-
-            string message;
-            while (true)
-            {
-                message = Console.ReadLine();
-
-                if (message.ToLower() == "exit")
-                {
-                    break;
-                }
-
-                // Invia il messaggio al server tramite il gestore dei messaggi in uscita
-                messageSender.SendMessage(message);
+                break;
             }
 
-            // Chiudi la connessione con il server
-            client.Close();
-
-            Console.WriteLine("Connection closed.");
+            byte[] sendData = Encoding.ASCII.GetBytes(message);
+            stream.Write(sendData, 0, sendData.Length);
         }
+
+        stream.Close();
+        client.Close();
+
+        Console.WriteLine("Connection closed.");
     }
 
-    // Classe per la gestione dell'invio dei messaggi al server
-    public class MessageSender
+    private static void ReceiveMessages()
     {
-        private StreamWriter writer;
-
-        public MessageSender(StreamWriter writer)
+        try
         {
-            this.writer = writer;
-        }
+            byte[] buffer = new byte[1024];
+            int bytesRead;
 
-        public void SendMessage(string message)
-        {
-            // Invia il messaggio al server e lo flusha per assicurarsi che venga inviato subito
-            writer.WriteLine(message);
-            writer.Flush();
-            Console.WriteLine("Message sent to the server");
-        }
-    }
-
-    // Classe per la gestione della ricezione dei messaggi dal server
-    public class MessageReceiver
-    {
-        private StreamReader reader{get;set;}
-        private bool receiving{get;set;}
-        public event EventHandler<string> MessageReceived{get;set;}
-
-        public MessageReceiver(StreamReader reader)
-        {
-            this.reader = reader;
-            this.receiving = false;
-        }
-
-        public void Start()
-        {
-            receiving = true;
-            while (receiving)
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
             {
-                try
-                {
-                    // Leggi il messaggio dal server
-                    string serverResponse = reader.ReadLine();
-                    if (serverResponse == null)
-                    {
-                        receiving = false; // Esci dal ciclo se non ci sono più messaggi
-                        break;
-                    }
-
-                    // Scatena l'evento per notificare il messaggio ricevuto
-                    OnMessageReceived(serverResponse);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An error occurred: " + ex.Message);
-                    receiving = false;
-                }
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: " + message);
             }
         }
-
-        protected virtual void OnMessageReceived(string message)
+        catch (Exception ex)
         {
-            MessageReceived?.Invoke(this, message);
+            Console.WriteLine("An error occurred: " + ex.Message);
         }
     }
 }
